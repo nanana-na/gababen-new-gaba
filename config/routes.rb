29a653d59af7a950b2776104e25decbb00c40e81
nanana-na <hiubadonakao@icloud.com>
2022-12-03 00:24:42 +0900

    user機能、home機能、post機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root :to => 'home#index'
+  resources :users
 end
